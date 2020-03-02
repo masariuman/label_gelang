8ff04a0b0fd0c38ce834f9a81bcb355d188a41e8
masariuman <masariuman@gmail.com>
2020-03-02 10:36:59 +0700

    awal

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Data;
+use PDF;
 
 class DataController extends Controller
 {
@@ -14,5 +15,27 @@ class DataController extends Controller
         return response()->json([
             'cari' => $data
 		]);
+    }
+
+    public function label($id)
+    {
+        //
+        $data = Data::where('no_rkm_medis', 'like', '%' . $id . '%')->first();
+		dd($data);
+    }
+
+    public function templateLabel()
+
+    {
+        $label = Data::first();
+        $data['label'] = $label;
+        // $count = count($label);
+        // $data['count'] = $count;
+        $data['today'] = date('d/m/Y');
+
+        $pdf = PDF::loadView('print.label', $data);
+        // return $pdf->stream();
+        return view('print.label');
+        // return $pdf->download('laporan-pdf.pdf');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,11 @@ Route::get('/', function () {
 });
 
 Route::post('/', 'DataController@cari');
+Route::get('/{id}/label', 'DataController@label');
+
+route::get('/templatelabel','DataController@templateLabel');
+route::get('/templategelangdewasa','DataController@templateGelangDewasa');
+route::get('/templategelanganak','DataController@templateGelangAnak');
+route::get('/templatetracer','DataController@templateTracer');
 
 
