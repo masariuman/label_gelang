e42acea357082907624be8de1cdce3cafb680806
masariuman <masariuman@gmail.com>
2020-04-09 16:23:43 +0700

    asdasd

--- resources/js/components/Track.js
+++ resources/js/components/Track.js
@@ -10,7 +10,7 @@ class Track extends Component {
             url: "/tracer/data",
             awalan: "TN.",
             tanggal_masuk: "",
-            peminjam: "%20"
+            peminjam: "%20",
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,40 +24,40 @@ class Track extends Component {
     getTodayDate() {
         var today = new Date();
         var dd = today.getDate();
-        var mm = today.getMonth()+1;
+        var mm = today.getMonth() + 1;
         var yyyy = today.getFullYear();
-        if(dd<10){
-            dd='0'+dd;
+        if (dd < 10) {
+            dd = "0" + dd;
         }
-        if(mm<10){
-            mm='0'+mm;
+        if (mm < 10) {
+            mm = "0" + mm;
         }
-        var terbalik = yyyy+'-'+mm+'-'+dd;
+        var terbalik = yyyy + "-" + mm + "-" + dd;
         return terbalik;
     }
 
     tanggalmasukChange(e) {
         this.setState({
-            tanggal_masuk: e.target.value
+            tanggal_masuk: e.target.value,
         });
     }
 
     awalanChange(e) {
         this.setState({
-            awalan: e.target.value
+            awalan: e.target.value,
         });
     }
 
     handleChange(e) {
         this.setState({
-            cari: e.target.value
+            cari: e.target.value,
         });
         // console.log(e.target.value);
     }
 
     peminjamChange(e) {
         this.setState({
-            peminjam: e.target.value
+            peminjam: e.target.value,
         });
         // console.log(e.target.value);
     }
@@ -66,9 +66,9 @@ class Track extends Component {
         e.preventDefault();
         axios
             .post("/tracer/data", {
-                cari: this.state.cari
+                cari: this.state.cari,
             })
-            .then(response => {
+            .then((response) => {
                 this.setState({
                     data: [response.data.cari],
                     cari: "",
@@ -76,15 +76,15 @@ class Track extends Component {
                     peminjam: "%20",
                 });
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error.message);
             });
     }
 
     getData() {
-        axios.get("/tracer/data").then(response => {
+        axios.get("/tracer/data").then((response) => {
             this.setState({
-                data: response.data.cari
+                data: response.data.cari.pasien,
             });
             // console.log(response.data.cari);
         });
@@ -92,43 +92,69 @@ class Track extends Component {
 
     renderCari() {
         if (!this.state.data[0]) {
-            return this.state.data.map(data => (
+            return this.state.data.map((data) => (
                 <div key="1">DATA TIDAK ADA</div>
             ));
         } else {
-            return this.state.data.map(data => (
-               
+            return this.state.data.map((data) => (
                 <tr key={data.NORM}>
                     <td>{data.NORM}</td>
-                    <td><input name="TANGGAL_MASUK" placeholder="Tanggal Masuk" type="date" className="form-control" required onChange={this.tanggalmasukChange} value={this.state.tanggal_masuk} /></td>
-                    <td><select name="AWALAN" id="exampleSelect" className="form-control" onChange={this.awalanChange}>
-                                    <option value="TN.">TN.</option>
-                                    <option value="NY.">NY.</option> 
-                                    <option value="NN.">NN.</option>
-                                    <option value="AN.">AN.</option>
-                                    <option value="BY.">BY.</option>
-                                    <option value="BY.NY">BY.NY</option>
-                                    </select></td>
+                    <td>
+                        <input
+                            name="TANGGAL_MASUK"
+                            placeholder="Tanggal Masuk"
+                            type="date"
+                            className="form-control"
+                            required
+                            onChange={this.tanggalmasukChange}
+                            value={this.state.tanggal_masuk}
+                        />
+                    </td>
+                    <td>
+                        <select
+                            name="AWALAN"
+                            id="exampleSelect"
+                            className="form-control"
+                            onChange={this.awalanChange}
+                        >
+                            <option value="TN.">TN.</option>
+                            <option value="NY.">NY.</option>
+                            <option value="NN.">NN.</option>
+                            <option value="AN.">AN.</option>
+                            <option value="BY.">BY.</option>
+                            <option value="BY.NY">BY.NY</option>
+                        </select>
+                    </td>
                     <td>{data.NAMA}</td>
-                    <td>{data.JENIS_KELAMIN === 1 ? "Laki-Laki" : "Perempuan"}</td>
+                    <td>
+                        {data.JENIS_KELAMIN === 1 ? "Laki-Laki" : "Perempuan"}
+                    </td>
                     {/* <td>{data.TEMPAT_LAHIR}</td> */}
                     <td>{data.TANGGAL_LAHIR}</td>
                     <td>{data.ALAMAT}</td>
-                    <td><input name="PEMINJAM" placeholder="Peminjam" type="text" className="form-control" required onChange={this.peminjamChange} value={this.state.peminjamChange} /></td>
                     <td>
-                      
-                        <a  onSubmit={this.handleSubmit}
+                        <input
+                            name="PEMINJAM"
+                            placeholder="Peminjam"
+                            type="text"
+                            className="form-control"
+                            required
+                            onChange={this.peminjamChange}
+                            value={this.state.peminjamChange}
+                        />
+                    </td>
+                    <td>
+                        <a
+                            onSubmit={this.handleSubmit}
                             // href={`/tracer/${data.NORM}/print`}
                             href={`/${data.NORM}/${this.state.awalan}/${this.state.tanggal_masuk}/${this.state.peminjam}/tracer`}
                             className="btn btn-success btn-xs"
                             target="_blank"
                         >
-                            
                             <i className="fa fa-print"></i> Cetak Tracer
                         </a>
                     </td>
                 </tr>
-                
             ));
         }
     }
@@ -193,17 +219,17 @@ class Track extends Component {
                             <table className="mb-0 table table-bordered">
                                 <thead>
                                     <tr>
-                                       <th>No Rekam Medis</th>
-                                       <th>Tanggal Masuk</th>
-                                       <th>Awalan</th>
-                                       <th>Nama Pasien</th>
-        
-                                       <th>Jenis Kelamin</th>
-                                       {/* <th>Tempat Lahir</th> */}
-                                       <th>Tanggal Lahir</th>
-                                       <th>Alamat</th>
-                                       <th>Peminjam</th>
-                                       <th>Aksi</th>
+                                        <th>No Rekam Medis</th>
+                                        <th>Tanggal Masuk</th>
+                                        <th>Awalan</th>
+                                        <th>Nama Pasien</th>
+
+                                        <th>Jenis Kelamin</th>
+                                        {/* <th>Tempat Lahir</th> */}
+                                        <th>Tanggal Lahir</th>
+                                        <th>Alamat</th>
+                                        <th>Peminjam</th>
+                                        <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>{this.renderCari()}</tbody>
